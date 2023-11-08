dffcd0fc22de3715ae7cb7ff4abec5649367fb89
김현중 <141725575+kim-hyun-joong@users.noreply.github.com>
2023-11-08 09:23:17 +0900

    push

--- DrChuck/src/main/java/com/smhrd/controller/DrLiveController.java
+++ DrChuck/src/main/java/com/smhrd/controller/DrLiveController.java
@@ -53,7 +53,12 @@ public class DrLiveController {
    }
 
    @PostMapping("/closeWebcam")
-   public ResponseEntity<String> stopWebcam(Model model) {
+   public ResponseEntity<String> stopWebcam(Model model, HttpSession session) {
+	   
+	   DrMember user = (DrMember) session.getAttribute("user");
+       String userId = user.getDmId();
+       String userSex = user.getDmSex();
+       
       // POST 방식으로 플라스크 애플리케이션으로 웹캠을 닫도록 요청을 보냅니다.
       RestTemplate restTemplate = new RestTemplate();
       String flaskAppUrl = "http://172.30.1.55:5000/stopWebcam"; // 플라스크 애플리케이션의 웹캠 닫기 엔드포인트 URL
@@ -62,9 +67,8 @@ public class DrLiveController {
       headers.setContentType(MediaType.APPLICATION_JSON);
 
       // 요청 본문 데이터, 필요에 따라 JSON 형식으로 데이터를 만들어 보낼 수 있습니다.
-      String requestBody = "{\"회원아이디\": \"123\"}";
 
-      HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+      HttpEntity<String> entity = new HttpEntity<>(headers);
 
       String response = restTemplate.postForObject(flaskAppUrl, entity, String.class);
 
